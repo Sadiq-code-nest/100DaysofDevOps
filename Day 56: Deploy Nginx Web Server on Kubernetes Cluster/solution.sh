45641feb74a2f8988c5f29af2d73418c013a3b9b
Sadiq-code-nest <sadiqict63@gmail.com>
2026-06-18 10:39:16 +0600

    Refactor Day 56: split into separate deployment.yml and service.yml

--- Day 56: Deploy Nginx Web Server on Kubernetes Cluster/solution.sh
+++ Day 56: Deploy Nginx Web Server on Kubernetes Cluster/solution.sh
@@ -2,45 +2,8 @@
 # DAY 56 - Deploy Nginx Web Server on Kubernetes Cluster
 # Run from jump host
 
-cat > nginx-deployment.yml << 'EOF'
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: nginx-deployment
-  labels:
-    app: nginx
-spec:
-  replicas: 3
-  selector:
-    matchLabels:
-      app: nginx
-  template:
-    metadata:
-      labels:
-        app: nginx
-    spec:
-      containers:
-      - name: nginx-container
-        image: nginx:latest
-        ports:
-        - containerPort: 80
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: nginx-service
-spec:
-  type: NodePort
-  selector:
-    app: nginx
-  ports:
-  - protocol: TCP
-    port: 80
-    targetPort: 80
-    nodePort: 30011
-EOF
-
-kubectl apply -f nginx-deployment.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
 
 # verify
 kubectl get deployment nginx-deployment
