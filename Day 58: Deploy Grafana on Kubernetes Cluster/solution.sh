797967d3cd154343f080f2f1149d32ebd88ed2da
Sadiq-code-nest <sadiqict63@gmail.com>
2026-06-18 10:39:16 +0600

    Refactor Day 58: split into separate deployment.yml and service.yml

--- Day 58: Deploy Grafana on Kubernetes Cluster/solution.sh
+++ Day 58: Deploy Grafana on Kubernetes Cluster/solution.sh
@@ -2,44 +2,11 @@
 # DAY 58 - Deploy Grafana on Kubernetes Cluster
 # Run from jump host
 
-cat > grafana.yml << 'EOF'
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: grafana-deployment-datacenter
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: grafana
-  template:
-    metadata:
-      labels:
-        app: grafana
-    spec:
-      containers:
-      - name: grafana
-        image: grafana/grafana
-        ports:
-        - containerPort: 3000
----
-apiVersion: v1
-kind: Service
-metadata:
-  name: grafana-service
-spec:
-  type: NodePort
-  selector:
-    app: grafana
-  ports:
-  - port: 3000
-    targetPort: 3000
-    nodePort: 32000
-EOF
-
-kubectl apply -f grafana.yml
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
 
 # verify
 kubectl get deployment grafana-deployment-datacenter
 kubectl get pods
 kubectl get svc grafana-service
+# Access: http://<node-ip>:32000
